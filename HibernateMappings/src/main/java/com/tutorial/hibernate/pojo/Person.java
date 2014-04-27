b90aa3672cebebcc200feca9194e78279f61927d
Jayram <jayram.excel@gmail.com>
2014-04-27 00:39:12 -0400

    Update

--- HibernateMappings/src/main/java/com/tutorial/hibernate/pojo/Person.java
+++ HibernateMappings/src/main/java/com/tutorial/hibernate/pojo/Person.java
@@ -1,17 +1,31 @@
 package com.tutorial.hibernate.pojo;
 
-public class Person
-{
-	private  int pid;
+public class Person {
+	private int pid;
 	private String pname;
-	private JobType  pjob;
+	private JobType pjob;
 
-	public void setPid(int n){ pid=n; }
-	public int getPid(){ return pid; }
+	public void setPid(int n) {
+		pid = n;
+	}
 
-	public void setPname(String s){ pname=s; }
-	public String getPname(){ return pname; }
+	public int getPid() {
+		return pid;
+	}
 
-	public void setPjob(JobType j){ pjob=j; }
-	public JobType getPjob(){ return pjob; }
+	public void setPname(String s) {
+		pname = s;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPjob(JobType j) {
+		pjob = j;
+	}
+
+	public JobType getPjob() {
+		return pjob;
+	}
 }
